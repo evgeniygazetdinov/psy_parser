1178bc282c4054188019b51bd556cca86a6affd5
evgesha <zplacesound@gmail.com>
2020-03-26 22:11:59 +0300

    my process today

--- main.py
+++ main.py
@@ -4,11 +4,29 @@ import requests
 import re
 
 URL = "https://www.b17.ru/forum/?f=102"
+HOST = 'https://www.b17.ru'
 
+def one_paginate():
+	pass
+
+
+def get_info_from_topic(topic):
+	page = requests.get(HOST+topic)
+	soup = BeautifulSoup(page.content)
+	topic_info = soup.find_all("table",{"class":"topic_post"})
+	return topic_info
+	
+def parse_one_table(table):
+	pass
+
+def extract_from_topic(topic):
+	print("EXTRACT")
+	info = get_info_from_topic(topic)
+	parse_one_table(info[0])
 
 
 def forum_checker(URL):
-	#get all from one paginate page
+	#get all from one paginate pagegit
 	forum_links = []
 	page = requests.get(URL)
 	data = page.content
@@ -22,7 +40,10 @@ def forum_checker(URL):
 	return res
 
 def main():
-	forum_checker(URL)
+	topics = forum_checker(URL)
+	for topic in topics[0:2]:
+		print('HERE')
+		extract_from_topic(topic)
 	print("this main")
 
 
