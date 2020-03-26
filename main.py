03b337c5faaa3c7b3d8d57f362b1b2e09a0ce576
evgesha <zplacesound@gmail.com>
2020-03-26 19:28:12 +0300

    work on links

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
 #!/usr/bin/env python
+from bs4 import BeautifulSoup, SoupStrainer
+import requests
+import re
+
+URL = "https://www.b17.ru/forum/?f=102"
 
 
+
+def forum_checker(URL):
+	#get all from one paginate page
+	forum_links = []
+	page = requests.get(URL)
+	data = page.content
+	soup = BeautifulSoup(data,"lxml")
+	for link in soup.find_all('a'):
+		forum_links.append(link.get('href'))
+	l = list(filter(None, forum_links))
+	res = [x for x in l if re.search("id", x)]
+	print(res)
+	print(len(res))
+	return res
 
 def main():
+	forum_checker(URL)
 	print("this main")
 
 
