b0c087bced0a92b86b2eff13f8b9e62b3d20b4b7
evgesha <zplacesound@gmail.com>
2020-03-27 11:56:36 +0300

    add targets

--- main.py
+++ main.py
@@ -3,41 +3,13 @@ from bs4 import BeautifulSoup, SoupStrainer
 import requests
 import re
 from collections import OrderedDict
-
-
+from lib.one_table import 
 
 URL = "https://www.b17.ru/forum/?f=102"
 HOST = 'https://www.b17.ru'
 
 def one_paginate():
 	pass
-
-
-def get_info_from_topic(topic):
-	page = requests.get(HOST+topic)
-	soup = BeautifulSoup(page.content,features="lxml")
-	topic_info = soup.find_all("table",{"class":"topic_post"})
-	return topic_info
-
-def get_topic_id(from_topic):
-	raw_topic = re.findall(r'\w{2}=\S\w{4}_\d+',str(from_topic))
-	if raw_topic:
-		topic_id = raw_topic[0].split('id="post_')
-		return topic_id[1]
-	return 
-
-
-def parse_one_table(table):
-	#extract values
-	#rebuild regexp
-	topic_id = get_topic_id(table)
-	print(topic_id)
-	
-
-def extract_from_topic(topic):
-	info = get_info_from_topic(topic)
-	for info_for_write in info:
-			parse_one_table(info_for_write)
 
 def forum_checker(URL):
 	#get all from one paginate pagegit
@@ -62,17 +34,51 @@ def forum_checker(URL):
 				forum_links[link.getText()] = link.get('href')
 	print(len(forum_links))
 	return forum_links
-	#l = list(filter(None, forum_links))
-	#res = [x for x in l if re.search("id", x)]
-	#print(res)
-	#print(len(res))
-	#return res
+
+
+def extract_from_topic(topic_link):
+	info = get_info_from_topic(topic_link)
+	print(topic_link)
+	for info_for_write in info:
+			parse_one_table(info_for_write)
+
+def get_topic_id(from_topic):
+	raw_topic = re.findall(r'\w{2}=\S\w{4}_\d+',str(from_topic))
+	if raw_topic:
+		topic_id = raw_topic[0].split('id="post_')
+		return topic_id[1]
+	return 		
+
+def find_quote_in_table(table):
+	pass
+
+def find_author(table):
+	pass
+
+def find_likes(table):
+	pass
+
+def insert_time_stamp(table):
+	pass
+
+def parse_one_table(table):
+	#extract values
+	#rebuild regexp
+	topic_id = get_topic_id(table)
+	quote = find_quote_in_table(table)
+	print(topic_id)	
+	
+def get_info_from_topic(topic):
+	page = requests.get(HOST+topic)
+	soup = BeautifulSoup(page.content,features="lxml")
+	topic_info = soup.find_all("table",{"class":"topic_post"})
+	return topic_info
+
 
 def main():
 	topics = forum_checker(URL)
-	print(topics)
-#	for topic in topics[0:1]:
-	#	extract_from_topic(topic)
+	for topic_name,topic_link in topics.items():
+		extract_from_topic(topic_link)
 
 
 
