a9643e9712470fd58b1f57b5e8531d66ed8c319e
evgesha <zplacesound@gmail.com>
2020-03-29 15:44:38 +0300

    before another

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import requests
 import re
 from collections import OrderedDict
 from lib.one_table_func import find_likes, get_topic_id, find_quote_in_table, get_number_post,parse_one_table 
+import time 
+from lib.change_ip import changer
+
 
 URL = "https://www.b17.ru/forum/?f=102"
 HOST = 'https://www.b17.ru'
@@ -41,35 +44,55 @@ def extract_from_topic(topic_link):
 			parse_one_table(info_for_write)
 
 
-def all_pagination(current_number_page):
-	#danger recursive_stuff
-	return links
+def all_numbers_in_page(url):
+	page = requests.get(HOST+url[0])
+	soup = BeautifulSoup(page.content,features="lxml")
+	paginated = soup.find("div",{"class":"page-list"})
+	all_links_in_current_page = paginated.find_all('a')
+	return all_links_in_current_page
 
 """
 #some realization
 def funk(page):
 	all_paginated = []
+	#return links from page
 	all_links = some_work()
 	all_paginated.append(all_links)
 	if all_links =="следуюшая страница":
 		all_paginated.append(funk(all_links[-1]))
-	return all_links
+	return all_paginated
 """
 
 
-def find_pages_in_topic(soup):
+def find_pages_in_topic(link):
 	#Target find entity which go to link return all links if last link equlize == "SLEDUUSHAYA STRANITSA" do work again
 	all_pages = []
-	paginated = soup.find("div",{"class":"page-list"})
-	current_number_page = paginated.find_all('a')
+	all_links = all_numbers_in_page(link)
+	if re.match(r'Следующая страница',str(((all_links[-1]).contents)[0])):
+		all_pages.append(find_pages_in_topic(all_links[-1].get('href')))
+	return all_pages
 	#if last link is next page 
 #	for page in current_number_page:
 		#if re.match(r'Следующая страница',str((page.contents)[0])):
 
-
-
-	
-
+def some_functional(topic_url):
+	links = []
+	res = []
+	page = requests.get(HOST+topic_url)
+	soup = BeautifulSoup(page.content,features="lxml")
+	if soup.find("div",{"class":"page-list"}):
+		f = soup.find("div",{"class":"page-list"})
+		a = f.find_all('a')
+		if re.match(r'Следующая страница',str(((a[-1]).contents)[0])):
+			print((a[-1]).get('href'))
+			some_functional((a[-1]).get('href'))
+			print('in recursive')
+			time.sleep(.700)
+			res.append((a[-1]).get('href'))
+		for i in a:
+			res.append((i).get('href'))
+		return res
+	return [topic_url]
 
 
 
@@ -77,10 +100,11 @@ def check_pagination(topics,topic_name,topic_url):
 	#change list_urls if url has pagination ==> add pagination link to list
 	#to match tricky way
 	#get older dict check paginate after push into new
-	page = requests.get(HOST+topic_url[0])
-	soup = BeautifulSoup(page.content,features="lxml")
-	if soup.find("div",{"class":"page-list"}):
-		topics[topic_name] = find_pages_in_topic(topic_url)
+	topic_links = some_functional(topic_url[0])
+	print('this result in topic')
+	print(topic_links)
+	print('^'*10)
+		#topics[topic_name] = find_pages_in_topic(topic_url)
 
 		#topics[topic_name].append()
 
@@ -96,11 +120,10 @@ def main():
 	topics = forum_checker(URL)
 	topics_with_pagination = OrderedDict()
 	for topic_name,topic_link in topics.items():
-		print(topic_name)
+		#fuck void func
 		check_pagination(topics_with_pagination,topic_name,topic_link)
 		#extract_from_topic(topic_link)
-
-
+	#changer()
 
 
 if __name__ == "__main__":
