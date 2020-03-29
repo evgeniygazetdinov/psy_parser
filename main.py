904f5c45b0a1fe8dad857a719c1624a100afb3a6
evgesha <zplacesound@gmail.com>
2020-03-29 08:07:08 +0300

    in work on nested links in pagination

--- main.py
+++ main.py
@@ -8,11 +8,6 @@ from lib.one_table_func import find_likes, get_topic_id, find_quote_in_table, ge
 URL = "https://www.b17.ru/forum/?f=102"
 HOST = 'https://www.b17.ru'
 
-def one_paginate():
-	pass
-
-
-
 
 def forum_checker(URL):
 	#get all from one paginate pagegit
@@ -46,38 +41,47 @@ def extract_from_topic(topic_link):
 			parse_one_table(info_for_write)
 
 
+def all_pagination(current_number_page):
+	#danger recursive_stuff
+	return links
 
-def parse_one_table(table):
-	soup = BeautifulSoup(str(table),"lxml")
-	#extract values
-	#rebuild regexp
-	# topic_id = get_topic_id(table)
-	# quote = find_quote_in_table(table)
-	# print(quote)
-	# print(topic_id)
-	# author = find_author(soup,table)
-	# print(author)	
-	likes = find_likes(soup)
-	number = get_number_post(soup)
-	print(number)
-	#print(likes)
-	#date = insert_time_stamp(soup)
-	#print(date)
+"""
+#some realization
+def funk(page):
+	all_paginated = []
+	all_links = some_work()
+	all_paginated.append(all_links)
+	if all_links =="следуюшая страница":
+		all_paginated.append(funk(all_links[-1]))
+	return all_links
+"""
 
 
-def
+def find_pages_in_topic(soup):
+	#Target find entity which go to link return all links if last link equlize == "SLEDUUSHAYA STRANITSA" do work again
+	all_pages = []
+	paginated = soup.find("div",{"class":"page-list"})
+	current_number_page = paginated.find_all('a')
+	#if last link is next page 
+#	for page in current_number_page:
+		#if re.match(r'Следующая страница',str((page.contents)[0])):
+
+
+
+	
 
 
 
 
-def check_pagination(topics,topics_name,topic_url):
+def check_pagination(topics,topic_name,topic_url):
 	#change list_urls if url has pagination ==> add pagination link to list
 	#to match tricky way
 	#get older dict check paginate after push into new
 	page = requests.get(HOST+topic_url[0])
 	soup = BeautifulSoup(page.content,features="lxml")
 	if soup.find("div",{"class":"page-list"}):
-		find_pages_in topic(soup)
+		topics[topic_name] = find_pages_in_topic(topic_url)
+
 		#topics[topic_name].append()
 
 
